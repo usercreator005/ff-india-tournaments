5e8a14c213832639fe19675775c128dd8e456c1f
usercreator005 <r07706048@gmail.com>
2026-01-26 02:53:14 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,22 +1,29 @@
+import { auth } from "./firebase.js";
+import {
+  GoogleAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
 const googleBtn = document.getElementById("googleLoginBtn");
 
-googleBtn.addEventListener("click", () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
+googleBtn.addEventListener("click", async () => {
+  const provider = new GoogleAuthProvider();
 
-  auth.signInWithPopup(provider)
-    .then(result => {
-      const user = result.user;
-      console.log("Logged in:", user.email);
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
 
-      // TEMP ROLE LOGIC (Phase 1)
-      if (user.email === "jarahul989@gmail.com") {
-        window.location.href = "creator.html";
-      } else {
-        // default user
-        window.location.href = "user.html";
-      }
-    })
-    .catch(err => {
-      alert(err.message);
-    });
+    console.log("Logged in:", user.email);
+
+    // TEMP ROLE LOGIC (Phase 1)
+    if (user.email === "jarahul989@gmail.com") {
+      window.location.href = "creator.html";
+    } else {
+      window.location.href = "user.html";
+    }
+
+  } catch (err) {
+    alert(err.message);
+    console.error(err);
+  }
 });
