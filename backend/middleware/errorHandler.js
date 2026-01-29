82947d95efb6c666a670d780252399ed10518113
usercreator005 <r07706048@gmail.com>
2026-01-29 09:06:09 +0530

    Create errorHandler.js

---
+++ backend/middleware/errorHandler.js
@@ -0,0 +1,8 @@
+module.exports = (err, req, res, next) => {
+  console.error("🔥 Error:", err.message);
+
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+};
