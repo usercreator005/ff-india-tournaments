acdb54ebbd59cd58e5e15b5984e189c488bae895
usercreator005 <r07706048@gmail.com>
2026-01-30 15:02:29 +0530

    Update rateLimiter.js

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,14 +1,12 @@
 const rateLimit = require("express-rate-limit");
 
-const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // 100 requests per IP
+module.exports = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 200,
   standardHeaders: true,
   legacyHeaders: false,
   message: {
     success: false,
-    message: "Too many requests. Please try again later."
-  }
+    msg: "Too many requests, please try again later",
+  },
 });
-
-module.exports = apiLimiter;
