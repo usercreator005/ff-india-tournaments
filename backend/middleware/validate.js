e2b22edcac96b51db9735b55f151af7bb62bd879
usercreator005 <r07706048@gmail.com>
2026-01-30 14:59:22 +0530

    Update validate.js

--- backend/middleware/validate.js
+++ backend/middleware/validate.js
@@ -1,11 +1,12 @@
-const { body, param } = require("express-validator");
+const { validationResult } = require("express-validator");
 
-exports.validateTournamentCreate = [
-  body("title").trim().isLength({ min: 3 }),
-  body("entryFee").isNumeric(),
-  body("maxSlots").isInt({ min: 1 }),
-];
-
-exports.validateObjectId = [
-  param("id").isMongoId()
-];
+module.exports = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array(),
+    });
+  }
+  next();
+};
