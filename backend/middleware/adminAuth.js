4a5fe1f4a6f6bebd4e4157f9f06569f3ebf11633
usercreator005 <r07706048@gmail.com>
2026-02-10 10:01:27 +0530

    Create adminAuth.js

---
+++ backend/middleware/adminAuth.js
@@ -0,0 +1,8 @@
+const adminAuth = (req, res, next) => {
+  if (!req.user || req.user.role !== "admin") {
+    return res.status(403).json({ msg: "Admin access only" });
+  }
+  next();
+};
+
+module.exports = adminAuth;
